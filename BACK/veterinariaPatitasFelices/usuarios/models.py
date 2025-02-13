e2291aa5ce13973099941a69ae61303d03ad51ea
Santiago Bertorello <santinbertorello@gmail.com>
2025-02-13 05:58:48 -0300

    se crea app usuario y prueba de modelo, (a evaluar) se propone cambiar y utilizar los campos del modelo del sistema de autenticación de Django. Se replantea proyeccion del trabajo, Se cargaran y migraran las demas apps y modelos para asi tener nocion de los permisos del cliente y del veterinario

--- BACK/veterinariaPatitasFelices/usuarios/models.py
+++ BACK/veterinariaPatitasFelices/usuarios/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.db import models
+
+class Usuario(models.Model):
+    
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    dni = models.CharField(max_length=8, unique=True)
+    fecha_nacimiento = models.DateField()
+    email = models.EmailField(unique=True)
+    direccion = models.CharField(max_length=200)
+
+    class Meta:
+        abstract = True
+
+class Cliente(Usuario):
+    user = models.OneToOneField('usuarios.CustomUser', on_delete=models.CASCADE, related_name='cliente')
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+
+class Medico(Usuario):
+    matricula = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return f"Dr. {self.nombre} {self.apellido}"
