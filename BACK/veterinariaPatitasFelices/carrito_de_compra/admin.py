cef1886ac089c37be557cc93d37dcfe8260ecc30
Santiago Bertorello <santinbertorello@gmail.com>
2025-02-18 19:46:07 -0300

    visualizacion de carrito de compra, item carrito y pedido. se agregan apps, carrito_de_compra y articulos en settings

--- BACK/veterinariaPatitasFelices/carrito_de_compra/admin.py
+++ BACK/veterinariaPatitasFelices/carrito_de_compra/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from carrito_de_compra.models import CarritoCompra, ItemCarrito, Pedido
 
+admin.site.register(CarritoCompra)
+admin.site.register(ItemCarrito)
+admin.site.register(Pedido)
 # Register your models here.
--- BACK/veterinariaPatitasFelices/veterinariaPatitasFelices/settings.py
+++ BACK/veterinariaPatitasFelices/veterinariaPatitasFelices/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'usuarios',
+    'articulos',
+    'carrito_de_compra',
 ]
 
 MIDDLEWARE = [
