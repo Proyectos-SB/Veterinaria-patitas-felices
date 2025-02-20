944ca43ffa16ddec866457baad2c6146a247291d
Santiago Bertorello <santinbertorello@gmail.com>
2025-02-20 11:45:02 -0300

    CREACION MODELO MASCOTA

--- BACK/veterinariaPatitasFelices/mascotas/models.py
+++ BACK/veterinariaPatitasFelices/mascotas/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Mascota(models.Model):
+    SEXO_MASCOTA = [
+        ('Macho', 'Macho'),
+        ('Hembra', 'Hembra'),
+    ]
+    nombre = models.CharField(max_length=100)
+    raza = models.CharField(max_length=100)
+    edad = models.IntegerField()
+    sexo = models.CharField(max_length=50, choices=SEXO_MASCOTA, default='-')
+
+    def __str__(self):
+        return self.nombre
