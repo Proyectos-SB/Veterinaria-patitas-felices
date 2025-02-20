d04710ec98a609f96306a6d1890cb5a23b6af44f
Santiago Bertorello <santinbertorello@gmail.com>
2025-02-20 11:30:28 -0300

    creacion de modelos contacto y faq

--- BACK/veterinariaPatitasFelices/contacto_y_faq/models.py
+++ BACK/veterinariaPatitasFelices/contacto_y_faq/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Contacto(models.Model):
+    
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField(max_length=70)
+    telefono = models.CharField(max_length=20) 
+    descripcion = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    respondido = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nombre + ' ' + self.apellido
+        
+
+class FAQ(models.Model):
+
+    pregunta = models.CharField(max_length=100)
+    descripcion = models.TextField()
