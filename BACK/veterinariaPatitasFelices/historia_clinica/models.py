95e397a63f9b43e616b12c8f04b25395d02550a1
Santiago Bertorello <santinbertorello@gmail.com>
2025-02-21 16:53:44 -0300

    modelo historia clinica, sin completar

--- BACK/veterinariaPatitasFelices/historia_clinica/models.py
+++ BACK/veterinariaPatitasFelices/historia_clinica/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from mascotas.models import Mascota
 # Create your models here.
+class HistoriaClinica(models.Model):
+    mascota = models.ForeignKey(Mascota, on_delete=models.CASCADE)
+    descripcion = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"Historia Clinica de {self.mascota.nombre}"
+   
