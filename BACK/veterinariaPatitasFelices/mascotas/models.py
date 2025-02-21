07fab4e95b0816b1f82212f17887944c7e5997c8
Santiago Bertorello <santinbertorello@gmail.com>
2025-02-21 16:52:25 -0300

    cambios modelo mascota: se agrega atributo peso

--- BACK/veterinariaPatitasFelices/mascotas/models.py
+++ BACK/veterinariaPatitasFelices/mascotas/models.py
@@ -5,11 +5,13 @@ class Mascota(models.Model):
     SEXO_MASCOTA = [
         ('Macho', 'Macho'),
         ('Hembra', 'Hembra'),
+        ('-', '-'),
     ]
     nombre = models.CharField(max_length=100)
     raza = models.CharField(max_length=100)
     edad = models.IntegerField()
-    sexo = models.CharField(max_length=50, choices=SEXO_MASCOTA, default='-')
+    sexo = models.CharField(max_length=20, choices=SEXO_MASCOTA, default='-')
+    peso = models.IntegerField(max_length=3)
 
     def __str__(self):
         return self.nombre
