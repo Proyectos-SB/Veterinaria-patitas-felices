87aa2625a6572f9c1ac59a172334d39588a9f453
Santiago Bertorello <santinbertorello@gmail.com>
2025-02-23 05:39:07 -0300

    se incorpora registro a Routes

--- FRONT/veterinaria-patitas-felices/src/app/app.routes.ts
+++ FRONT/veterinaria-patitas-felices/src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
+import { RegistroComponent } from './features/auth/registro/registro.component';
 
 export const routes: Routes = [
     {path: "index", component: LandingComponent},
+    {path: "registro", component: RegistroComponent},
     {path:"", redirectTo:"/index", pathMatch:"full"},
+    
+    
+
     
 ];
