0f90812e5c38d17361678d77796ac460893574fc
Santiago Bertorello <santinbertorello@gmail.com>
2025-02-24 20:43:07 -0300

    views carrito de compra.

--- BACK/veterinariaPatitasFelices/veterinariaPatitasFelices/urls.py
+++ BACK/veterinariaPatitasFelices/veterinariaPatitasFelices/urls.py
@@ -4,5 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/usuarios/', include('usuarios.urls')),
+    path('api/usuarios/', include('usuarios.urls')),    
+    path('api/vacunas/', include('vacunas.urls')),
+     path('api/carrito/', include('carrito_de_compra.urls')),
 ]
