ec66b60055a43105e1a8607b8190c5866b7e68cb
Santiago Bertorello <santinbertorello@gmail.com>
2025-02-24 14:05:31 -0300

    se crean las vistas del registro y login

--- BACK/veterinariaPatitasFelices/usuarios/views.py
+++ BACK/veterinariaPatitasFelices/usuarios/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import ClienteRegistroSerializer, LoginSerializer
+
+class ClienteRegistroAPIView(APIView):
+    def post(self, request):
+        serializer = ClienteRegistroSerializer(data=request.data)
+        if serializer.is_valid():
+            cliente = serializer.save()
+            return Response({
+                "message": "Cliente registrado con éxito",
+                "cliente": serializer.data
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+class LoginAPIView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            email = serializer.validated_data['email']
+            password = serializer.validated_data['password']
+            user = authenticate(email=email, password=password)
+            if user:
+                # Generar el token JWT
+                refresh = RefreshToken.for_user(user)
+                return Response({
+                    'access': str(refresh.access_token),
+                    'refresh': str(refresh)
+                })
+            return Response({"error": "Credenciales inválidas"}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
