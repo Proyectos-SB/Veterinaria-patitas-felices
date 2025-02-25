6c6fc206dbcc9a0e85713db9ecace91af2f60c3f
Santiago Bertorello <santinbertorello@gmail.com>
2025-02-25 01:23:52 -0300

    Urls app vacunas

--- BACK/veterinariaPatitasFelices/veterinariaPatitasFelices/urls.py
+++ BACK/veterinariaPatitasFelices/veterinariaPatitasFelices/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/usuarios/', include('usuarios.urls')),
     path('api/carrito/', include('carrito_de_compra.urls')),
+    path('api/vacunas/', include('vacunas.urls')),
 ]
