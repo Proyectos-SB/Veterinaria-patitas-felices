d842f219dcd53e83d19cff9c70a6fe2d4c30d89a
Santiago Bertorello <santinbertorello@gmail.com>
2025-02-25 00:58:16 -0300

    se crean urls para app carrito, se integra a url proyecto principal

--- BACK/veterinariaPatitasFelices/veterinariaPatitasFelices/urls.py
+++ BACK/veterinariaPatitasFelices/veterinariaPatitasFelices/urls.py
@@ -5,4 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/usuarios/', include('usuarios.urls')),
+    path('api/carrito/', include('carrito_de_compra.urls')),
 ]
