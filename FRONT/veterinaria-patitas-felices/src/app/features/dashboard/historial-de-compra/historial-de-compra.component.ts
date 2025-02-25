64a930d0559d220810ae15e93fd76cf04190e0e1
Santiago Bertorello <santinbertorello@gmail.com>
2025-02-25 16:42:29 -0300

    se crean componentes del dashboard: mascota, historial-de-compra, perfil

---
+++ FRONT/veterinaria-patitas-felices/src/app/features/dashboard/historial-de-compra/historial-de-compra.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-historial-de-compra',
+  standalone: true,
+  imports: [],
+  templateUrl: './historial-de-compra.component.html',
+  styleUrl: './historial-de-compra.component.css'
+})
+export class HistorialDeCompraComponent {
+
+}
