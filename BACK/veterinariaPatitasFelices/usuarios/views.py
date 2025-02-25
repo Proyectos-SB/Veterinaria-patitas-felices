a41e9fd7d127482940a95da52b84795e13c452ce
Santiago Bertorello <santinbertorello@gmail.com>
2025-02-25 16:37:57 -0300

    se agregan vistas para el traer y actualizar el usuario

--- BACK/veterinariaPatitasFelices/usuarios/views.py
+++ BACK/veterinariaPatitasFelices/usuarios/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
 
-from rest_framework import status, permissions
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import ClienteRegistroSerializer, LoginSerializer
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import update_session_auth_hash
+
 
 class ClienteRegistroAPIView(APIView):
     def post(self, request):
         serializer = ClienteRegistroSerializer(data=request.data)
         if serializer.is_valid():
-            cliente = serializer.save()
+            usuario  = serializer.save()
             return Response({
                 "message": "Cliente registrado con éxito",
                 "cliente": serializer.data
@@ -35,3 +38,39 @@ class LoginAPIView(APIView):
                 })
             return Response({"error": "Credenciales inválidas"}, status=status.HTTP_401_UNAUTHORIZED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+
+class ObtenerUsuarioAutenticadoAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        """
+        Obtener los datos del usuario autenticado.
+        """
+        usuario = request.user  # Obtiene el usuario autenticado
+        serializer = ClienteRegistroSerializer(usuario)  # Serializa los datos del usuario
+        return Response(serializer.data)
+
+class ActualizarUsuarioAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request):
+        """
+        Actualizar los datos del usuario autenticado.
+        """
+        usuario = request.user  # Obtiene el usuario autenticado
+        serializer = ClienteRegistroSerializer(usuario, data=request.data, partial=True)  # Usa partial=True para permitir actualizaciones parciales
+
+        if serializer.is_valid():
+            serializer.save()  # Guarda los cambios
+            # Si se actualizó la contraseña, actualiza la sesión del usuario
+            if 'password' in request.data:
+                update_session_auth_hash(request, usuario)  # Esto evita que el usuario se desconecte después de cambiar su contraseña
+            return Response({
+                "message": "Usuario actualizado correctamente.",
+                "usuario": serializer.data
+            }, status=status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  
