ce8e38f6d6f31cc6650a3855a46d56befe1f17f1
Santiago Bertorello <santinbertorello@gmail.com>
2025-02-25 17:10:45 -0300

    se incorpora servicio de autenticacion login. en core/auth/service/login.service . se eliminan de componente login servicio prueba.

--- FRONT/veterinaria-patitas-felices/src/app/core/services/auth.service.ts
+++ FRONT/veterinaria-patitas-felices/src/app/core/services/auth.service.ts
@@ -1,25 +1,30 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LoginRequest } from '../../features/auth/models/loginRequest.model';
-import { catchError, Observable, tap ,throwError} from 'rxjs';
+import { catchError, Observable, tap ,throwError,BehaviorSubject} from 'rxjs';
 import { Router } from '@angular/router';
+import { LoginResponse } from '../../features/auth/models/loginResponse.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private LOGIN_URLs = 'http://localhost:3000/login';
+  private LOGIN_URLs = 'http://localhost:8000/api/usuarios/login/';
   private tokenKey = 'authToken';
+  private authStatusSubject = new BehaviorSubject<boolean>(this.hasToken());
+  public authStatus$ = this.authStatusSubject.asObservable();
      
   constructor(private http: HttpClient,private router: Router) {}
 
   login(usuario: LoginRequest): Observable<LoginResponse> {
     return this.http.post<LoginResponse>(this.LOGIN_URLs, usuario).pipe(
       tap(response => {
-        if (response.token) {
-          console.log(response.token);
+        if (response) {
+          console.log(response.access);
+          console.log(response.refresh);
           console.log('El Logeo fue un éxito');
-          this.setToken(response.token);
+          this.setToken(response.access);
         } else {
           console.error("Authentication failed");
         }
@@ -31,14 +36,29 @@ export class AuthService {
     );
   }
 
-  private setToken(token: string): void {
-    localStorage.setItem(this.tokenKey, token);
-  }
+
+  private setToken(access: string): void {
+  localStorage.setItem(this.tokenKey, access);
+}
   private getToken(): string | null {
     return localStorage.getItem(this.tokenKey); 
   }
-  isAuthenticated(): boolean {
-    return !!this.getToken(); // Devuelve true si hay token, false si no
+  getAuthHeaders(): { headers: HttpHeaders } {
+    const token = this.getToken();
+    return {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      })
+    };
+  }
+  private hasToken(): boolean {
+    return !!this.getToken();
+  }
+  
+  // Devuelve el estado de autenticación como Observable
+  isAuthenticated(): Observable<boolean> {
+    return this.authStatus$;
   }
   logOut(): void {
     localStorage.removeItem(this.tokenKey);
