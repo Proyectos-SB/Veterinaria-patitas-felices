80d2bfa15eef7b512f0bfdba99e1826c0e6b30c5
Santiago Bertorello <santinbertorello@gmail.com>
2025-02-25 17:47:27 -0300

    se modifica auth.guards con un enfoque mas reactivo y eficiente. suscribimos al estado mas actualizado con authStatus$. Se protege ruta dashboard.

--- FRONT/veterinaria-patitas-felices/src/app/app.routes.ts
+++ FRONT/veterinaria-patitas-felices/src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
 import { RegistroComponent } from './features/auth/registro/registro.component';
 import { LoginComponent } from './features/auth/login/login.component';
+import { DashboardPrincipalComponent } from './features/dashboard/dashboard-principal/dashboard-principal.component';
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
     {path: "index", component: LandingComponent},
     {path: "registro", component: RegistroComponent},
     {path: "login", component: LoginComponent},
+    {path:"dashboard", component: DashboardPrincipalComponent,canActivate: [authGuard]},
     {path:"", redirectTo:"/index", pathMatch:"full"},
     
     
